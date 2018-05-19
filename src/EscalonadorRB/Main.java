b5747067fd6a71d0789956f47ed9c4880c989407
mhi <mauricio_ishida@hotmail.com>
2018-05-18 21:41:55 -0300

    Criados novos arquivos p/ RB

---
+++ src/EscalonadorRB/Main.java
@@ -0,0 +1,10 @@
+package EscalonadorRB;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
